23507776d79b3bf52826c6937b7c672a7dc2c686
Shamshad <sheikh15-12126@diu.edu.bd>
2021-03-23 21:38:11 +0600

    import libraries and bg using tkinter

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import tkinter.messagebox
+import sqlite3
+
+class Product:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("WAREHOUSE INVENTORY SALES PURCHASE MANAGEMENT SYSTEM")
+        self.root.geometry("1325x690")
+        self.root.config(bg="grey")
+
+
+if __name__== '__main__':
+    root=Tk()
+    application=Product(root)
+    root.mainloop()
