dba3781af24b304ad3c26b705106e3d4056be8e9
Shamshad <sheikh15-12126@diu.edu.bd>
2021-03-23 22:26:08 +0600

    create frame

--- main.py
+++ main.py
@@ -9,8 +9,21 @@ class Product:
         self.root.geometry("1325x690")
         self.root.config(bg="grey")
 
+        MainFrame = Frame(self.root,bg="red")
+        MainFrame.grid()
+
+        HeadFrame=Frame(MainFrame,bd=1,padx=50,pady=10,
+                        bg='white',relief=RIDGE)
+        HeadFrame.pack(side=TOP)
+
+        self.ITitle=Label(HeadFrame,font=('arial',50,'bold'),
+                          fg='red',text="Warehouse Inventory Sales Purchase",
+                           bg="white")
+        self.ITitle.grid()
+
 
 if __name__== '__main__':
     root=Tk()
     application=Product(root)
     root.mainloop()
+
